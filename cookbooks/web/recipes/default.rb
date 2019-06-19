6b626702db77a606ba62c9d6925e3e840b535da0
Felix Breidenstein <felix@codemonauts.com>
2019-06-19 15:33:22 +0200

    Switched to Ubuntu Bionic and PHP7.2

--- cookbooks/web/recipes/default.rb
+++ cookbooks/web/recipes/default.rb
@@ -4,17 +4,16 @@ package "mysql-client"
 package "php-imagick";
 package "php-soap";
 package "php-xml";
-package "php7.0-cli";
-package "php7.0-curl";
-package "php7.0-fpm";
-package "php7.0-gd";
-package "php7.0-intl";
-package "php7.0-mbstring";
-package "php7.0-mcrypt";
-package "php7.0-mysql";
-package "php7.0-opcache";
-package "php7.0-xml";
-package "php7.0-zip";
+package "php7.2-cli";
+package "php7.2-curl";
+package "php7.2-fpm";
+package "php7.2-gd";
+package "php7.2-intl";
+package "php7.2-mbstring";
+package "php7.2-mysql";
+package "php7.2-opcache";
+package "php7.2-xml";
+package "php7.2-zip";
 
 # Delete default configuration files
 file_array = ['/etc/nginx/sites-enables/default',
@@ -27,7 +26,7 @@ file_array.each do |this_file|
 end
 
 # Install aws-elasticache plugin
-cookbook_file '/usr/lib/php/20151012/amazon-elasticache-cluster-client.so' do
+cookbook_file '/usr/lib/php/20170718/amazon-elasticache-cluster-client.so' do
   source 'amazon-elasticache-cluster-client.so'
   owner 'root'
   group 'root'
@@ -35,7 +34,7 @@ cookbook_file '/usr/lib/php/20151012/amazon-elasticache-cluster-client.so' do
   action :create
 end
 
-file '/etc/php/7.0/mods-available/aws-memcache.ini' do
+file '/etc/php/7.2/mods-available/aws-memcache.ini' do
   content 'extension=amazon-elasticache-cluster-client.so'
   owner 'root'
   group 'root'
@@ -48,7 +47,7 @@ execute 'enable aws-memcache module' do
   action :run
 end
 
-service "php7.0-fpm" do
+service "php7.2-fpm" do
   action [:stop, :disable]
 end
 
